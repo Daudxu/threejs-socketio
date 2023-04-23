82da96707a883089ec6ac5048398e478fb49adc1
xudan <875126243@qq.com>
2023-04-23 09:39:14 +0800

    fix add emoji

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,178 @@
 // VERSION
 const VERSION = import.meta.env.VITE_VERSION
 
+const EMOGI = [
+    {
+      title: "[呵呵]",
+      image: require("../assets/images/emoji/smilea_thumb.gif"),
+    },
+    {
+      title: "[嘻嘻]",
+      image: require("../assets/images/emoji/tootha_thumb.gif"),
+    },
+    {
+      title: "[哈哈]",
+      image: require("../assets/images/emoji/laugh.gif"),
+    },
+    {
+      title: "[可爱]",
+      image: require("../assets/images/emoji/tza_thumb.gif"),
+    },
+    {
+      title: "[可怜]",
+      image: require("../assets/images/emoji/kl_thumb.gif"),
+    },
+    {
+      title: "[挖鼻屎]",
+      image: require("../assets/images/emoji/kbsa_thumb.gif"),
+    },
+    {
+      title: "[吃惊]",
+      image: require("../assets/images/emoji/cj_thumb.gif"),
+    },
+    {
+      title: "[害羞]",
+      image: require("../assets/images/emoji/shamea_thumb.gif"),
+    },
+    {
+      title: "[挤眼]",
+      image: require("../assets/images/emoji/zy_thumb.gif"),
+    },
+    {
+      title: "[闭嘴]",
+      image: require("../assets/images/emoji/bz_thumb.gif"),
+    },
+    {
+      title: "[鄙视]",
+      image: require("../assets/images/emoji/bs2_thumb.gif"),
+    },
+    {
+      title: "[爱你]",
+      image: require("../assets/images/emoji/lovea_thumb.gif"),
+    },
+    {
+      title: "[泪]",
+      image: require("../assets/images/emoji/sada_thumb.gif"),
+    },
+    {
+      title: "[偷笑]",
+      image: require("../assets/images/emoji/heia_thumb.gif"),
+    },
+    {
+      title: "[亲亲]",
+      image: require("../assets/images/emoji/qq_thumb.gif"),
+    },
+    {
+      title: "[生病]",
+      image: require("../assets/images/emoji/sb_thumb.gif"),
+    },
+    {
+      title: "[太开心]",
+      image: require("../assets/images/emoji/mb_thumb.gif"),
+    },
+    {
+      title: "[懒得理你]",
+      image: require("../assets/images/emoji/ldln_thumb.gif"),
+    },
+    {
+      title: "[右哼哼]",
+      image: require("../assets/images/emoji/yhh_thumb.gif"),
+    },
+    {
+      title: "[左哼哼]",
+      image: require("../assets/images/emoji/zhh_thumb.gif"),
+    },
+    {
+      title: "[嘘]",
+      image: require("../assets/images/emoji/x_thumb.gif"),
+    },
+    {
+      title: "[衰]",
+      image: require("../assets/images/emoji/cry.gif"),
+    },
+    {
+      title: "[委屈]",
+      image: require("../assets/images/emoji/wq_thumb.gif"),
+    },
+    {
+      title: "[吐]",
+      image: require("../assets/images/emoji/t_thumb.gif"),
+    },
+    {
+      title: "[打哈气]",
+      image: require("../assets/images/emoji/k_thumb.gif"),
+    },
+    {
+      title: "[抱抱]",
+      image: require("../assets/images/emoji/bba_thumb.gif"),
+    },
+    {
+      title: "[怒]",
+      image: require("../assets/images/emoji/angrya_thumb.gif"),
+    },
+    {
+      title: "[疑问]",
+      image: require("../assets/images/emoji/yw_thumb.gif"),
+    },
+    {
+      title: "[馋嘴]",
+      image: require("../assets/images/emoji/cza_thumb.gif"),
+    },
+    {
+      title: "[拜拜]",
+      image: require("../assets/images/emoji/88_thumb.gif"),
+    },
+    {
+      title: "[思考]",
+      image: require("../assets/images/emoji/sk_thumb.gif"),
+    },
+    {
+      title: "[汗]",
+      image: require("../assets/images/emoji/sweata_thumb.gif"),
+    },
+    {
+      title: "[困]",
+      image: require("../assets/images/emoji/sleepya_thumb.gif"),
+    },
+    {
+      title: "[睡觉]",
+      image: require("../assets/images/emoji/sleepa_thumb.gif"),
+    },
+    {
+      title: "[钱]",
+      image: require("../assets/images/emoji/money_thumb.gif"),
+    },
+    {
+      title: "[失望]",
+      image: require("../assets/images/emoji/sw_thumb.gif"),
+    },
+    {
+      title: "[酷]",
+      image: require("../assets/images/emoji/cool_thumb.gif"),
+    },
+    {
+      title: "[花心]",
+      image: require("../assets/images/emoji/hsa_thumb.gif"),
+    },
+    {
+      title: "[哼]",
+      image: require("../assets/images/emoji/hatea_thumb.gif"),
+    },
+    {
+      title: "[鼓掌]",
+      image: require("../assets/images/emoji/gza_thumb.gif"),
+    },
+    {
+      title: "[晕]",
+      image: require("../assets/images/emoji/dizzya_thumb.gif"),
+    },
+    {
+      title: "[悲伤]",
+      image: require("../assets/images/emoji/bs_thumb.gif"),
+    }
+  ];
+
 export  {
-    VERSION
+    VERSION,
+    EMOGI
 }
